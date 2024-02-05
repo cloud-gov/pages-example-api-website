ee58b66201544b105a466758de2bf915f3595e7f
Ephraim-G <ephraim.gross@gsa.gov>
2024-02-05 18:31:45 -0500

    everything

--- data.js
+++ data.js
@@ -1,17 +1,20 @@
-fetch("https:cors-anywhere.herokuapp.com/https:cfpyapi.app.cloud.gov/get_table", { 
-headers: {
-    'x-requested-with': 'XMLHttpRequest'
-}
+fetch("https:cfpyapi.app.cloud.gov/get_table").then((data)=>{
+    return data.json();
+}).then((objectData)=>{
+    console.log(objectData[0].title);
+    let tableData=""; // All data inside the table
+    objectData.map((values)=>{
+        tableData+=` <tr>
+        <td>${values['Acquiring Institution']}</td>
+        <td>${values['Bank Name']}</td>
+        <td>${values.Cert}</td>
+        <td>${values.City}</td>
+        <td>${values['Closing Date']}</td>
+        <td>${values.Fund}</td>
+        <td>${values.State}</td>
+</tr>`;
+    });
+    document.getElementById("table_body").innerHTML=tableData
+}).catch((error)=>{
+    console.log(error);
 })
-.then(response => {
-    if (!response.ok) {
-        throw new Error('HTTP error: ' + response.status);
-    }
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-})
-.catch(function () {
-    console.log('Request Failed');
-});
