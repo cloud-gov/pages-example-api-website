783a7bfb3eac5a289d9a6c36837b0cd016261654
Ephraim-G <ephraim.gross@gsa.gov>
2024-02-05 17:11:41 -0500

    proxy server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({
+  target: 'http://cfpyapi.app.cloud.gov',
+  changeOrigin: true,
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app.listen(3000);
